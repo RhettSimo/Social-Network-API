70449e0f9636b2d15a56611e42a50a6141131e23
RhettSimo <simonsonrhett1337@gmail.com>
2023-09-04 11:35:38 -0500

    undid reaction schema change (redundant)

--- models/Thought.js
+++ models/Thought.js
@@ -29,36 +29,6 @@ const thoughtSchema = new Schema(
   }
 );
 
-const reactionSchema = new Schema (
-  {
-     reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-     },
-     reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280
-     },
-     username: {
-      type: String,
-      required: true,
-     },
-     createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-     },
-  },
-  {
-      toJSON: {
-          virtuals: true,
-          getters: true
-      },
-      id: false,
-  }
-)
-
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length
 })
