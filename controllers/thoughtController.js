a5b25ab0e77695d96220a9ed49317e78cef8a54c
RhettSimo <simonsonrhett1337@gmail.com>
2023-09-04 12:22:12 -0500

    changed wording of comments in some areas and fixed api bug

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,7 @@ const { User, Thought } = require('../models');
 module.exports = {
   // Get all thoughts
   getAllThoughts(req, res) {
-    Thought.find()
+    Thought.findOne()
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
